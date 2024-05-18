b1704171fd2fb76ffee350e8748e26e5c7258059
SquaringHard <147991384+SquaringHard@users.noreply.github.com>
2024-05-18 08:38:08 +0700

    Update OutputGen.bat
    add option to not compile .cpp files

--- OutputGen.bat
+++ OutputGen.bat
@@ -1,4 +1,5 @@
 @echo Generating outputs
-@g++ -o OutputGen OutputGen.cpp study_in_pink2.cpp -I . -std=c++11
+@set /p input = Compile *.cpp? Y/N 
+@IF "%input%" == "Y" (@g++ -o OutputGen OutputGen.cpp study_in_pink2.cpp -I . -std=c++11)
 @OutputGen.exe
 @pause
